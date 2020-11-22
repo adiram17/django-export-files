b160a5fe3e325208f2d08ad83d275d42a1c3c62b
adiramadhan <adi.ramadhan@sigma.co.id>
2020-11-22 10:10:08 +0700

    Initial commit

---
+++ myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+from import_export.admin import ExportActionMixin
+
+# Register your models here.
+class BookAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('title', 'description', 'author', 'year')
+
+admin.site.register(Book, BookAdmin)
